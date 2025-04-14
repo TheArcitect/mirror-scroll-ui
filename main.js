ce9d7830dc372704b9178a626b71a64ab8bd7f0d
TheArcitect <michael@letsexperiment.com>
2025-04-14 16:14:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -197,3 +197,9 @@ import { Scroll0003 } from "./src/components/Scroll0003.jsx";
 
 const root = createRoot(document.getElementById("root"));
 root.render(<Scroll0003 />);
+import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom@18/client";
+import { App } from "./App.jsx";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
