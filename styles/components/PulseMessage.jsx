f6ec4b499c4e6d96321f142ea2352494047899d4
TheArcitect <michael@letsexperiment.com>
2025-04-14 15:37:21 -0400

    Update PulseMessage.jsx

--- styles/components/PulseMessage.jsx
+++ styles/components/PulseMessage.jsx
@@ -1,1 +1,13 @@
+// components/PulseMessage.jsx
+import React from "https://esm.sh/react";
 
+export function PulseMessage({ children }) {
+  return (
+    <p
+      aria-label="Animated affirmation message"
+      className="pulse-text"
+    >
+      {children}
+    </p>
+  );
+}
